3d823ab7a1b60f807ca67f4123dc095de50929aa
Ibrahim Ali <sun_moon545@yahoo.com>
2019-03-09 08:34:13 +0200

    Add solutions

---
+++ src/algorithms/CutTheSticks.java
@@ -0,0 +1,130 @@
+package algorithms;
+
+import java.util.*;
+
+/*
+You are given a number of sticks of varying lengths. You will iteratively cut the sticks into smaller sticks, discarding the shortest pieces until there are none left. At each iteration you will determine the length of the shortest stick remaining, cut that length from each of the longer sticks and then discard all the pieces of that shortest length. When all the remaining sticks are the same length, they cannot be shortened so discard them.
+
+Given the lengths of  sticks, print the number of sticks that are left before each iteration until there are none left.
+
+For example, there are  sticks of lengths . The shortest stick length is , so we cut that length from the longer two and discard the pieces of length . Now our lengths are . Again, the shortest stick is of length , so we cut that amount from the longer stick and discard those pieces. There is only one stick left, , so we discard that stick. Our lengths are .
+
+Function Description
+
+Complete the cutTheSticks function in the editor below. It should return an array of integers representing the number of sticks before each cut operation is performed.
+
+cutTheSticks has the following parameter(s):
+
+arr: an array of integers representing the length of each stick
+Input Format
+
+The first line contains a single integer , the size of .
+The next line contains  space-separated integers, each an  where each value represents the length of the  stick.
+
+Output Format
+
+For each operation, print the number of sticks that are present before the operation on separate lines.
+
+Constraints
+
+Sample Input 0
+
+6
+5 4 4 2 2 8
+Sample Output 0
+
+6
+4
+2
+1
+Explanation 0
+
+sticks-length        length-of-cut   sticks-cut
+5 4 4 2 2 8             2               6
+3 2 2 _ _ 6             2               4
+1 _ _ _ _ 4             1               2
+_ _ _ _ _ 3             3               1
+_ _ _ _ _ _           DONE            DONE
+Sample Input 1
+
+8
+1 2 3 4 3 3 2 1
+Sample Output 1
+
+8
+6
+4
+1
+Explanation 1
+
+sticks-length         length-of-cut   sticks-cut
+1 2 3 4 3 3 2 1         1               8
+_ 1 2 3 2 2 1 _         1               6
+_ _ 1 2 1 1 _ _         1               4
+_ _ _ 1 _ _ _ _         1               1
+_ _ _ _ _ _ _ _       DONE            DONE
+
+ */
+public class CutTheSticks {
+
+    // Complete the cutTheSticks function below.
+    static int[] cutTheSticks(int[] arr) {
+        List<Integer> data = new ArrayList<>();
+        Arrays.sort(arr);
+        int minimum = arr[0];
+        int i = 0, j = 0;
+        int wholeLength = arr.length;
+        data.add(arr.length);
+        while (i < arr.length) {
+            for (; j < arr.length; j++) {
+                arr[j] -= minimum;
+                if (arr[j] < 1) {
+                    wholeLength--;
+                    i++;
+
+                }
+            }
+            j = i;
+            if (wholeLength == 0)
+                break;
+            data.add(wholeLength);
+        }
+
+        int[] result = new int[data.size()];
+        for (int k = 0; k < data.size(); k++) {
+                result[k] = data.get(k);
+        }
+        return result;
+
+//        Map<Integer, Integer> count = new HashMap<>();
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (count.get(arr[i]) == null) {
+//                count.put(arr[i], 1);
+//            } else {
+//                count.put(arr[i], count.get(arr[i]) + 1);
+//            }
+//        }
+//        List<Integer> sortedKeysList = new ArrayList<>(count.keySet());
+//        Collections.sort(sortedKeysList);
+//        int[] result = new int[sortedKeysList.size()];
+//        result[0] = arr.length;
+//        int length = arr.length;
+//        for (int i = 1; i < sortedKeysList.size(); i++) {
+//            length -= count.get(sortedKeysList.get(i - 1));
+//            result[i] = length;
+//        }
+//        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        //System.out.println(Arrays.toString(cutTheSticks(new int[]{1, 2, 3, 4, 3, 3, 2, 1})));
+        //System.out.println(Arrays.toString(cutTheSticks(new int[]{1, 2, 3})));
+        //System.out.println(Arrays.toString(cutTheSticks(new int[]{ 8, 7, 6, 4, 3, 2})));
+        //System.out.println(Arrays.toString(cutTheSticks(new int[]{8, 8, 14 ,10, 3, 5 ,14 ,12})));
+        System.out.println(Arrays.toString(cutTheSticks(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 45})));
+        //System.out.println(Arrays.toString(cutTheSticks(new int[]{5, 4, 4, 2, 2, 8})));
+    }
+}
---
+++ src/algorithms/LibraryFine.java
@@ -0,0 +1,73 @@
+package algorithms;
+
+/*
+Your local library needs your help! Given the expected and actual return dates for a library book, create a program that calculates the fine (if any). The fee structure is as follows:
+
+If the book is returned on or before the expected return date, no fine will be charged (i.e.: .
+If the book is returned after the expected return day but still within the same calendar month and year as the expected return date, .
+If the book is returned after the expected return month but still within the same calendar year as the expected return date, the .
+If the book is returned after the calendar year in which it was expected, there is a fixed fine of .
+Charges are based only on the least precise measure of lateness. For example, whether a book is due January 1, 2017 or December 31, 2017, if it is returned January 1, 2018, that is a year late and the fine would be .
+
+Function Description
+
+Complete the libraryFine function in the editor below. It must return an integer representing the fine due.
+
+libraryFine has the following parameter(s):
+
+d1, m1, y1: returned date day, month and year
+d2, m2, y2: due date day, month and year
+Input Format
+
+The first line contains  space-separated integers, , denoting the respective , , and  on which the book was returned.
+The second line contains  space-separated integers, , denoting the respective , , and  on which the book was due to be returned.
+
+Constraints
+
+Output Format
+
+Print a single integer denoting the library fine for the book received as input.
+
+Sample Input
+
+9 6 2015
+6 6 2015
+Sample Output
+
+45
+Explanation
+
+Given the following dates:
+Returned:
+Due:
+
+Because , we know it is less than a year late.
+Because , we know it's less than a month late.
+Because , we know that it was returned late (but still within the same month and year).
+
+Per the library's fee structure, we know that our fine will be . We then print the result of  as our output.
+ */
+public class LibraryFine {
+
+    // Complete the libraryFine function below.
+    static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+
+        if (y2 > y1)
+            return 0;
+
+        if (y1 > y2)
+            return 10000;
+        else if (m1 > m2)
+            return (m1 - m2) * 500;
+        else if (d1 > d2 && m1 >= m2)
+            return (d1 - d2) * 15;
+        else return 0;
+
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println(libraryFine(28, 2, 2015, 15, 4, 2015 ));
+    }
+}
