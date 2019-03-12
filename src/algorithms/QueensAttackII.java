8e0ceeece1d5324968c1b939df6b66fef96d3c72
Ibrahim Ali <sun_moon545@yahoo.com>
2019-03-12 10:13:29 +0200

    Add solutions

--- src/algorithms/QueensAttackII.java
+++ src/algorithms/QueensAttackII.java
@@ -93,20 +93,67 @@ public class QueensAttackII {
 
     // Complete the queensAttack function below.
     static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
-        int total = 0;
-        total = ((n - r_q) + (n - c_q)) * 2;
+        int total;
+        int positiveDiagonal = Math.abs(r_q - c_q);
+        int negativeDiagonal = Math.abs(r_q + c_q - n - 1);
+        int totalDiagonal = 2 * n - 2 - positiveDiagonal - negativeDiagonal;
+        total = n * 2 - 2;
+        total += totalDiagonal;
+
+        int row, column;
 
         if (k > 0)
             for (int i = 0; i < k; i++) {
+                row = obstacles[i][0];
+                column = obstacles[i][1];
 
+                if (row == r_q && column == c_q)
+                    continue;
+
+                // If same row
+                if (row == r_q) {
+                    total -= n - (column + 1);
+                }
+                // If same column
+                else if (column == c_q) {
+                    total -= n - (row + 1);
+                }
+                // It's diagonal
+                else {
+                    // Check if on the same diagonal
+                    if (Math.abs(r_q - row) == Math.abs(c_q - column)) {
+                        if (column == n || row == n) {
+                            total--;
+
+                        } else if (row > r_q) {
+
+                            total -= Math.abs(n - column - (n - row < n - column ? n - row : 0));
+
+                        } else total -= Math.abs(n - +1 - row);
+                    }
+                }
             }
 
-        return 0;
+        return total;
     }
 
 
     public static void main(String[] args) {
 
-        System.out.println(Math.sqrt((4)));
+//        2N−|x−y|−|x+y−N−1|.
+
+        int x = 4, y = 3;
+        int n = 5;
+
+        System.out.println((int)Math.sqrt(8));
+
+        //System.out.println(queensAttack(8, 0, 4, 3, new int[][]{}));
+        System.out.println(queensAttack(5, 4, 4, 3, new int[][]{
+                {5, 5},
+                {4, 2},
+                {2, 3},
+                {3, 4}
+        }));
+
     }
 }
